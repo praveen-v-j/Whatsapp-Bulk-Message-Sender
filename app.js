009aef5051bdf825744401fc5d35681c039faac9
praveen3164 <53088586+praveen3164@users.noreply.github.com>
2023-04-01 18:04:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -24,16 +24,15 @@ fs.createReadStream('contacts.csv')
 
 let counter = { fails: 0, success: 0 }
 
-/*
 const SESSION_FILE_PATH = './whatsapp-session.json';
-let sessionCfg;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionCfg = require(SESSION_FILE_PATH);
-}
-const client = new Client({
-    session: sessionCfg
-});
-*/
+
+// let sessionCfg;
+// if (fs.existsSync(SESSION_FILE_PATH)) {
+//     sessionCfg = require(SESSION_FILE_PATH);
+// }
+// const client = new Client({
+//     session: sessionCfg
+// });
 
 // equivalent to
 const client = new Client({
@@ -63,12 +62,30 @@ const userInput = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
-  
+let isLoggedIn = false;
+
   userInput.on('line', async (input) => {
     // if (input.toLowerCase() === 'send') {
     //   await deploy_all();
     //   // code to send messages here
     // } else 
+    if (input.toLowerCase() === 'send') {
+        if (!isLoggedIn) {
+          console.log('Please login first');
+          return;
+        }
+         console.log('Sending messages...');
+        }
+        // Add your code to send messages here
+    //   } else if (input.toLowerCase() === 'login') {
+    //     if (isLoggedIn) {
+    //       console.log('Already logged in');
+    //       return;
+    //     }
+    //     await client.waitForConnect();
+    //     isLoggedIn = true;
+    //     console.log('Logged in!');
+    //   }
     if (input === 'dub') {
         const contacts = fs.readFileSync('contacts.csv', 'utf-8').split('\n').filter(Boolean);
         const uniqueContacts = [...new Set(contacts)];
@@ -76,15 +93,42 @@ const userInput = readline.createInterface({
         console.log('Duplicate contacts removed!');
     } else if (input.toLowerCase() === 'clear') {
       console.clear();
-    } else if (input === 'logout') {
-      client.logout();
-      console.log('Account Was Logout Successfully!')
+    } else if (input.toLowerCase() === 'logout') {
+        try {
+            if (!isLoggedIn) {
+                console.log('No Account Found');
+                return;
+            } else {
+                client.logout(0);
+                console.log('Account ')
+            }
+          } catch (err) {
+            console.error('No Account Login');
+            return;
+          }
+          process.exit(0);
     } else if (input === 'exit') {
-      console.log('Ok Bye ;-)');
-      process.exit(0);
+        console.log('Ok Bye 😉')
+        process.exit(0);        
+    } else if (input === 'code') {
+        try {
+            if (!isLoggedIn) {
+              await client.initialize();
+              isLoggedIn = true;
+              console.log('Logged in!');
+            } else {
+              console.log('You are already logged in!');
+            }
+          } catch (err) {
+            console.error('Initialization failed');
+          }
+
     } else {
-      console.log('Invalid command. Please enter either "send", "exit", "logout" or "clear".');
-    } 
+        console.log(`Invalid command: ${input}`);
+      } 
+      // } else {
+    //   console.log('Invalid command. Please enter either "send", "exit", "logout" or "clear".');
+    // } 
   });  
 
 client.on('auth_failure', msg => {
